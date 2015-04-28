6cb10f26e64efb69a5af330a3f12e4f72d78092a
Oliver Schneider <oschneid@cs.ubc.ca>
2015-04-28 11:57:55 -0700

    Initial commit
    Added test application (clicking and dragging a circle) with required
    build information.

---
+++ js/app.js
@@ -0,0 +1,60 @@
+/** @jsx React.DOM */
+
+var ClickDrag = React.createClass({
+	getInitialState : function () {
+		return {
+					x: 0.5,
+					y: 0.5,
+					r: 50,
+					dragging: false
+		}
+	},
+
+	handleMouseUp : function(event) {
+		this.setState({dragging:false});
+	},
+
+	handleMouseDown : function(event) {
+		this.setState({dragging:true});
+		this.handleMouseMove(event);
+	},
+
+
+	handleMouseMove : function(event) {
+
+		if (this.state.dragging) {
+			var xScale = d3.scale.linear()
+						.domain([0, 1])
+						.range([0, this.props.width]);
+			var yScale = d3.scale.linear()
+						.domain([0,1])
+						.range([0,this.props.height]);
+
+			var newX = xScale.invert(event.clientX);
+			var newY = xScale.invert(event.clientY);
+
+	 		this.setState({x:newX, y:newY});
+		}
+			
+	},
+
+	render : function() {
+
+			var xScale = d3.scale.linear()
+						.domain([0, 1])
+						.range([0, this.props.width]);
+			var yScale = d3.scale.linear()
+						.domain([0,1])
+						.range([0,this.props.height]);
+
+		return (
+				<svg id="chart" width={this.props.width} height={this.props.height} onMouseDown={this.handleMouseDown} onMouseMove={this.handleMouseMove} onMouseUp={this.handleMouseUp}>
+					<circle cx={xScale(this.state.x)} cy={yScale(this.state.y)} fill="black" r={this.state.r}></circle>
+				</svg>
+				);
+		}
+
+	});
+
+
+		React.render(<ClickDrag width="500" height="500" />, document.getElementById('mount-point'));
