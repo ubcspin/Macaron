d09cef9271bad6e3267fec190aff312fc9b4b632
Oliver Schneider <oschneid@cs.ubc.ca>
2015-07-28 13:39:24 -0700

    Added timeline x scale

--- app/playhead.jsx
+++ app/playhead.jsx
@@ -18,7 +18,8 @@ var PlayHead = React.createClass({
 	      height: 20,
 	      width:'100%',
 	      playheadWidth: 20,
-	      background:'#EEEEEE'
+	      background:'#EEEEEE',
+	      axisTickLength:7
 	    }
 	},
 
@@ -33,7 +34,8 @@ var PlayHead = React.createClass({
 			background:this.props.background
 		};
 
-
+		var scaleX = this.props.scaleX;
+		var axisTickLength = this.props.axisTickLength;
         var x = this.props.scaleX(this.props.currentTime);
         var playheadWidth = this.props.playheadWidth;
         var h = this.props.height;
@@ -45,6 +47,45 @@ var PlayHead = React.createClass({
 
 			<div onMouseDown={this._handleMouseDown} ref="divWrapper" style={divStyle}>
 				<svg width="100%" height="100%">
+						{scaleX.ticks().map(function(tick, idx) {
+
+								//tick line
+								var lineProps = {
+									stroke:'black',
+									strokeWidth:0.5
+								};
+								lineProps['x1'] = scaleX(tick);
+								lineProps['x2'] = scaleX(tick);
+								lineProps['y1'] = 0;
+								lineProps['y2'] = 0 + axisTickLength;
+								var line = React.DOM.line(lineProps);
+
+								//tick label
+								var labelProps = {
+									fontSize:10,
+									textAlign:'center',
+									width:'100px',
+									className:'unselectable'
+
+								};
+								labelProps['x'] = scaleX(tick)-2.5*tick.toString().length;
+								labelProps['y'] = axisTickLength+10;
+								var label = React.DOM.text(labelProps, tick);
+								//console.log(label);
+								// console.log(label.getDOMNode());
+								// label.setAttribute('x', scaleX(tick)-label.getBBox().width);
+								//console.log(label);
+
+
+								return (<g key={idx}>
+											{line}
+											{label}
+										</g>);
+
+
+						})
+						}
+
 					<polygon points={playheadPoints} fill={this.props.playheadFill} />
 				</svg>
 			</div>
