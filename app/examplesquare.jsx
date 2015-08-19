c544151462e01b12e1be5a115cd002f42ef60dd0
Oliver Schneider <oschneid@cs.ubc.ca>
2015-08-19 15:46:00 -0700

    Example waveforms now displayed

--- app/examplesquare.jsx
+++ app/examplesquare.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Reflux from 'reflux';
+import d3 from 'd3';
 
 var ExampleStore = require('./stores/examplestore.js');
-
+var WaveformPathMixin = require('./util/waveformpathmixin.js');
 
 var ExampleSquare = React.createClass({
+	mixins: [
+		WaveformPathMixin,
+		Reflux.listenTo(ExampleStore.store,"onExampleChange")
+	],
 	
 	propTypes: {
 		exampleName : React.PropTypes.string.isRequired,
@@ -16,12 +22,32 @@ var ExampleSquare = React.createClass({
 	//       selectedBorder:10
 	       width:100,
 	       height:50,
-	//       // visColor:'#FFDDAD',
-	//       // background:"#FAFAFA",
-	//       // resolution:4000,
-	//       // maxFrequencyRendered:250,
-	//       // limitFrequencies:true
+	      visColor:'#FFDDAD',
+	      // background:"#FAFAFA",
+	      resolution:3000,
+	      maxFrequencyRendered:125,
+	      limitFrequencies:true
 	    }
+	},
+
+	onExampleChange: function(examples) {
+
+		var example = examples.examples[this.props.exampleName];
+		var scaleY = d3.scale.linear()
+                    .domain( [-1, 1]) // return value from sine
+                    .range([0, this.props.height]);
+
+        var scaleX = d3.scale.linear()
+                    .domain( [0, example.duration]) // return value from sine
+                    .range([0, this.props.width]);
+		this._visPath = this.computeWaveformPath(example,
+			scaleX, scaleY,
+			this.props.resolution, this.props.maxFrequencyRendered, this.props.limitFrequencies);
+	},
+
+	componentWillMount: function () {
+	    this._visPath = "";
+	    this.onExampleChange(ExampleStore.store.getInitialState());
 	},
 
 	_handleClick : function(e) {
@@ -41,7 +67,11 @@ var ExampleSquare = React.createClass({
 		};
 
 		return (
-			<div className={className} onClick={this._handleClick} style={styles}/>
+			<div className={className} onClick={this._handleClick} style={styles}>
+				<svg width="100%" height="100%">
+					<path stroke={this.props.visColor} strokeWidth="0.5" fill="none" d={this._visPath} />
+				</svg>
+			</div>
 			);
 	}
 
