af43f40a1b976ec4a37775017ecde7283d2ed6c7
Ben <ben.clark456@gmail.com>
2016-05-06 14:21:41 -0700

    Added the Save/Load Pop-up window, and starting working on the Save WAV feature

--- app/stores/saveloadstore.js
+++ app/stores/saveloadstore.js
@@ -16,21 +16,84 @@ var saveLoadStore = Reflux.createStore({
 	listenables: [saveLoadActions],
 
 	onSave() {
-		var data_json = JSON.stringify(VTIconStore.store.getInitialState()["main"], null, 2);
-		LogStore.actions.log("SAVE_"+data_json);
-		var data = 'data:text/json;charset=utf8,' + encodeURIComponent(data_json);
-		// window.location.assign(url);
-   		// window.open(url, '_blank');
-   		// window.focus();
 
-   		var a = document.createElement('a');
-		a.setAttribute('href', data);
-  		a.setAttribute('download', "icon.crumb");
-		// a.innerHTML = 'download JSON';
-	
-		document.body.appendChild(a);
-  		a.click();
-  		document.body.removeChild(a);
+		// Here you can specify how you'd like the files to be named
+		var jsonFileName = "my_waveform.json";
+		var wavFileName = "my_waveform.wav";
+
+		// This function closes the Save window when the "close" button is clicked.
+		var closeSaveWindow = function() {
+			document.body.removeChild(linksContainer);
+			document.body.removeChild(shadow);
+		}
+
+		/**
+		 * Creating the Pop-Up Style Container for Download Links
+		 */
+		var linksContainer = document.createElement("div");
+		linksContainer.id = "save-links-container";
+		linksContainer.classList.add("save-link-container");
+
+		var closeLink = document.createElement("a");
+		closeLink.id = "save-link-close-button";
+		closeLink.classList.add("close");
+		closeLink.innerHTML = "X";
+		closeLink.addEventListener("click", function(){closeSaveWindow();});
+
+		var linksHeader = document.createElement("H2");
+		linksHeader.id = "save-link-header";
+		linksHeader.classList.add("links-header");
+		linksHeader.innerHTML = "Save Your Macaron Waveform";
+
+		var shadow = document.createElement("div");
+		shadow.id = "links-box-shadow";
+		shadow.classList.add("link-box-shadow");
+
+
+		/**
+		 *  Creating the JSON file for download
+		 */
+		var dataJSON = JSON.stringify(VTIconStore.store.getInitialState()["main"], null, 2);
+		var fullDataJSON = "data:text/json;charset=utf8," + dataJSON;
+		var dataBlob = new Blob([fullDataJSON], {type: "text/JSON"});
+		dataBlob.name = jsonFileName;
+		var dataURL = URL.createObjectURL(dataBlob);
+
+		var jsonDownloadLink = document.createElement("a");
+		jsonDownloadLink.id = "json-download-link";
+		jsonDownloadLink.classList.add("download-link");
+		jsonDownloadLink.setAttribute("value", "Download");
+		jsonDownloadLink.setAttribute("href", dataURL);
+		jsonDownloadLink.setAttribute("download", jsonFileName);
+		jsonDownloadLink.innerHTML = "download Macaron file (JSON)";
+
+		/**
+		 * TODO: Implement WAV download
+		 */
+		var wavDownloadLink = document.createElement("a");
+		wavDownloadLink.id = "wav-download-link";
+		wavDownloadLink.classList.add("download-link");
+		wavDownloadLink.setAttribute("href", " ");
+		wavDownloadLink.setAttribute("download", wavFileName);
+		wavDownloadLink.innerHTML = "download Waveform File (WAV)";
+
+		/**
+		 * Now just append it all together!
+		 */
+		linksContainer.appendChild(closeLink);
+		linksContainer.appendChild(linksHeader);
+
+		var spacer = document.createElement("br");
+		var linksText = document.createElement("p");
+		linksText.id = "links-text";
+		linksText.appendChild(jsonDownloadLink);
+		linksText.appendChild(spacer);
+		linksText.appendChild(wavDownloadLink);
+
+		linksContainer.appendChild(linksText);
+		document.body.appendChild(linksContainer);
+		document.body.appendChild(shadow);
+
 	},
 
 	onLoadMacaronFile(file) {
