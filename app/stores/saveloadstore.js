eadbf300ec3f350993dfcb5fb8f6d0bb6526858c
Ben <ben.clark456@gmail.com>
2016-05-18 15:12:10 -0700

    I think I'm about to make some big changes... Committing in case of reverts...

--- app/stores/saveloadstore.js
+++ app/stores/saveloadstore.js
@@ -67,9 +67,6 @@ var saveLoadStore = Reflux.createStore({
 		jsonDownloadLink.setAttribute("download", jsonFileName);
 		jsonDownloadLink.innerHTML = "download Macaron file (JSON)";
 
-		/**
-		 * TODO: Implement WAV download
-		 */
 		var wavBuffer = this.generateWavFile();
 
 		var wavBlob = new Blob([wavBuffer], {type: "audio/wav"});
@@ -220,9 +217,11 @@ var saveLoadStore = Reflux.createStore({
 				var freqParams = iconStore.parameters.frequency.data;
 
         var range = Math.pow(2, this.bitDepth - 1) - 2;
-									// minus 2 to avoid any clipping.
+									// subtract 2 to avoid any clipping.
 
 				var phaseShift = 0;
+				var previousOffset1 = 0;
+				var previousOffset2 = 0;
 
         // calculate the speaker displacement at each frame
         //  emulating a sinewave here...
@@ -232,19 +231,30 @@ var saveLoadStore = Reflux.createStore({
 
 					var amp = getCurrentAmplitude(t, ampParams);
 					var ft = getCurrentFT(t, freqParams); // Integral of freq over t
-					var needsShift = needsPhaseShift(t, freqParams);
-					if (needsShift) {
-						//phaseShift = computePhaseShift(t, freqParams, phaseShift);
-				  }
 
 					var vol = range * amp;
+					vol = equalize(t, freqParams, vol);
 					var angle = Math.sin((2 * Math.PI * ft) + phaseShift);
-          var oscOffset = parseInt(Math.round(vol * angle));
+          var oscOffset = Math.round(vol * angle);
+
+					// // Fix any discontinuities
+					// var cutoff = 200;
+					// var needsShift = Math.abs(previousOffset1 - oscOffset) > cutoff;
+					// if (needsShift) {
+					// 	console.log('dunnit');
+					// 	phaseShift = computePhaseShift(previousOffset1, previousOffset2);
+					// 	angle = Math.sin((2 * Math.PI * ft) + phaseShift);
+	        //   oscOffset = parseInt(Math.round(vol * angle));
+					// 	console.log(phaseShift);
+				  // }
+					// previousOffset2 = previousOffset1;
+					// previousOffset1 = oscOffset;
 
           // Now if the value being written is negative, convert it to signed.
           if (oscOffset < 0) {
             oscOffset = ~(Math.abs(oscOffset));
           }
+
 
 
 					// Range - Offset = WAV encoding of Offset... Weird!
@@ -270,14 +280,26 @@ var saveLoadStore = Reflux.createStore({
 
 	onLoadMacaronFile(file) {
 		var reader = new FileReader();
+		reader.fileName = file.name;
 
 		reader.onload = function(e) {
-			var data = reader.result;
-			LogStore.actions.log("LOAD_"+data);
-			VTIconStore.actions.setVTIcon(JSON.parse(data), "main");
+			console.log(reader);
+    	var fn = e.target.fileName;
+
+			if (isWavFile(reader, fn)) {
+				alert('loading wave file: "' + fn + '"');
+				loadWAVFile(reader);
+			}
+			else if(isJSONFile(reader, fn)) {
+				loadJSONFile(reader);
+			}
+			else {
+				alert("the file appears to be a format that Macaron doesn't recognize");
+			}
+
 		};
 
-		reader.readAsText(file); //assumes 'utf8'
+		reader.readAsBinaryString(file); //assumes 'utf8'
 	}
 
 });
@@ -323,6 +345,39 @@ var saveLoadStore = Reflux.createStore({
 	*   time t based on the users keyframes created in the "Frequency"
 	*    pane of the Macaron editor.
 	**/
+var getCurrentFrequency = function(t, freqData) {
+
+	// First find the frequency at time t...
+	var frequency = 0;
+
+	for (var j=0; j<freqData.length; j++) {
+
+		if ((t <= freqData[0].t) && (j == 0)) {
+			frequency = freqData[j].value;
+
+		} else if ((t < freqData[j].t) && (t >= freqData[j-1].t)) {
+			var df = freqData[j].value - freqData[j-1].value;
+			var dt = freqData[j].t - freqData[j-1].t;
+			frequency = freqData[j-1].value + ((t - freqData[j-1].t)*(df/dt));
+
+		} else {
+			frequency = freqData[j].value;
+		}
+
+	}
+
+	return frequency;
+
+}
+
+
+/**
+ *  getCurrentFT computes the integral of frequency over time from the
+ *   previous keyframe out to the current time.
+ *
+ *  This is needed to exactly compute the displacement of a speaker
+ *   that is generating a soundwave of changing frequency.
+ **/
 var getCurrentFT = function(t, freqData) {
 
 	var ft = 0; // default
@@ -330,18 +385,12 @@ var getCurrentFT = function(t, freqData) {
 	for (var j=0; j<freqData.length; j++) {
 
 		// Case 1: t is before the first keyframes
-		if ((t <= freqData[0].t) && (j ==0)) {
+		if ((t <= freqData[0].t) && (j==0)) {
 			ft = freqData[0].value * (t/1000);
 		}
 
 		// Case 2: t is between two keyframes
 		else if ((t < freqData[j].t) && (t >= freqData[j-1].t)) {
-
-			// var fc = freqData[j-1].value; // Carrier frequency
-			// var Df = freqData[j].value - freqData[j-1].value; // Max change in frequency
-			// var Vm0 = (freqData[j].t - freqData[j-1].t) / fc; // Initial V mod
-			// var Vm = t; // mod function of time
-			// var freq = (fc + ((Df/Vm0) * Vm));
 
 			var dfTotal = freqData[j].value - freqData[j-1].value;
 			var dtTotal = freqData[j].t - freqData[j-1].t;
@@ -360,32 +409,29 @@ var getCurrentFT = function(t, freqData) {
 
 	} // End of the frequency calculations
 
-	return ft;
-}
-
-
-
-
-
-/**
- *  needsPhaseShift returns a boolean indicating whether or not the outputted
- *   waveform has a discontinuity large enough to require some sort of
- *    transformation. This occurs when there is a change in frequency over time
- **/
-var needsPhaseShift = function(t, freqData) {
-
-	var needsShift = false; //stub
+	var previousFT = 0;
 
 	for (var j=0; j<freqData.length; j++) {
-		if (freqData[j].t == t) {
-			needsShift = true;
-			alert('it happened...');
+
+		if (j == 0) {
+			previousFT += freqData[0].t * freqData[0].value;
 		}
+
+		else if (t >= freqData[j].t) {
+			break;
+		}
+
+		else {
+			var dt = freqData[j].t - freqData[j-1].t;
+			var freqSum = freqData[j].value + freqData[j-1].value;
+			previousFT += 0.5 * dt * freqSum;
+		}
+
+
 	}
 
-	return needsShift;
+	return ft + previousFT;
 }
-
 
 
 
@@ -396,14 +442,209 @@ var needsPhaseShift = function(t, freqData) {
  *    change do not align. This function will return the value of offset
  *     that will realign the waveform after a change in frequency.
  **/
-var computePhaseShift = function(t, freqData, oldPhaseShift) {
+var computePhaseShift = function(oldOffset, oldOldOffset) {
 
-	var tOld = t - (1/44100); // Assuming sampleRate = 44100
-	var ftOld = getCurrentFT(tOld, freqData);
-	var ftNew = getCurrentFT(t, freqData);
-	var phaseShift = (ftOld + oldPhaseShift) / ftNew;
+	var A = Math.pow(2, 8);
+	var phaseConst = Math.asin(Math.abs(oldOffset/A));
 
-	return phaseShift;
+	if (oldOffset > A) {
+		phaseConst = ~(Math.abs(phas));
+	}
+
+	phaseConst = phaseConst % (2 * Math.PI);
+
+	var dtExact = Math.cos(phaseConst)
+	var dtApprox = oldOffset - oldOldOffset;
+
+	var derivativesMatch = (dtExact * dtApprox) >= 0;
+
+	if(!derivativesMatch) {
+		phaseConst = phaseConst + Math.PI;
+	}
+
+
+	return phaseConst;
+}
+
+
+
+
+/**
+ *  the Equalize function will scale the volume to adjust for "sweet-spots"
+ *   in a given actuator. This code exactly emulates the algorithm in the
+ *    soundgen.jsx file written by Oliver.
+ **/
+var equalize = function(t, freqData, volume) {
+
+	// First find the frequency at time t...
+	var frequency = getCurrentFrequency(t, freqData);
+
+	// Now scale it...
+	var pf = 0;
+	var scaleFactor = 1;
+	if (frequency < 300) {
+		pf = (frequency - 50) / 250; // proportion of way btwn 50 and 300
+		scaleFactor = pf * 4 + 1; // proportion of gain difference, here 24
+	} else {
+		pf = (frequency - 300) / 200; // proportion of way btwn 300 and 550
+		scaleFactor = ((1 - pf) * 3) + 2; // proportion of gain difference
+	}
+
+	var equalizedVolume = volume / scaleFactor;
+
+	return equalizedVolume;
+}
+
+
+
+/**
+ * isWavFile determines whether or not a provided file is actually an
+ *  appropriate WAV file (with readable bit-depth, correct file format,
+ *   useful number of channels, etc.)
+ **/
+var isWavFile = function(r, fn) {
+
+	var result = false;
+
+	var header = r.result.charAt(0) + r.result.charAt(1);
+	header += r.result.charAt(2) + r.result.charAt(3);
+	header += r.result.charAt(8) + r.result.charAt(9);
+	header += r.result.charAt(10) + r.result.charAt(11);
+
+	var L = (Math.pow(2,24)*r.result.charCodeAt(7));
+	L += (Math.pow(2,16)*r.result.charCodeAt(6));
+	L += (Math.pow(2,8)*r.result.charCodeAt(5));
+	L += r.result.charCodeAt(4);
+
+	// Make sure we're actually looking at a WAV file.
+	var headerOK = (header == "RIFFWAVE");
+	var lengthOK = ((L - r.result.length) < 5);
+	var fileExOK = (fn.indexOf('.wav') >= 0);
+
+	if (headerOK && lengthOK && fileExOK) {
+		result = true;
+	}
+
+	return result;
+}
+
+/**
+ *  isJSONFile determines if the provided file is, in fact, a JSON file
+ *   that can be understood by the Macaron app.
+ **/
+var isJSONFile = function(r, fn) {
+	return(false);
+	//stuby stub
+}
+
+
+
+/**
+ *  loadWAVFile takes the contents of a WAV file and loads an approximation
+ *   of that file's waveform into the Macaron editor.
+ **/
+var loadWAVFile = function(r) {
+
+	// First let's extract the length of the "format" chunk
+	var fmtChunkL = Math.pow(2,24) * r.result.charCodeAt(19);
+	fmtChunkL += Math.pow(2,16) * r.result.charCodeAt(18);
+	fmtChunkL += Math.pow(2,8) * r.result.charCodeAt(17);
+	fmtChunkL += r.result.charCodeAt(16);
+
+	// We'll also need to know how many channels
+	var nChan = Math.pow(2,8) * r.result.charCodeAt(23);
+	nChan += r.result.charCodeAt(22);
+
+	// Then the sample rate
+	var sampRate = Math.pow(2,24) * r.result.charCodeAt(27);
+	sampRate += Math.pow(2,16) * r.result.charCodeAt(26);
+	sampRate += Math.pow(2,8) * r.result.charCodeAt(25);
+	sampRate += r.result.charCodeAt(24);
+
+	// And also the bit depth
+	var depth = Math.pow(2,8) * r.result.charCodeAt(35);
+	depth += r.result.charCodeAt(34);
+
+	// Compue the number of bits in each sample
+	var sampleSize = (depth * nChan) / 8;
+
+	// Now, pull all that data out into an array we can math on...
+	var wavData = new Array(r.result.length);
+	//Int8Array.apply(null, wavData);
+	for (var i=0; i<r.result.length; i++) {
+		var t = i + fmtChunkL;
+		wavData[i] = r.result.charCodeAt(t);
+	}
+
+	var nSamplePoints = 20;
+	var meshWidth = 3000 / nSamplePoints;
+
+	VTIconStore.actions.selectVTIcon("main");
+
+	for (var i=0; i<nSamplePoints; i++) {
+
+		var tMid = Math.round((1000 * i * (3/nSamplePoints)) + (0.5 * 1000 * (3/nSamplePoints)));
+
+		var meshLower = Math.round(tMid - (meshWidth/2));
+		var meshUpper = Math.round(tMid + (meshWidth/2));
+
+		var wavChunk = wavData.slice(meshLower, meshUpper);
+
+		console.log(wavChunk);
+
+		var aVal = estimateAmplitude(wavChunk) / (Math.pow(2,8) - 2);
+		var fVal = estimateFrequency(12);
+		VTIconStore.actions.newKeyframe("amplitude", tMid, aVal, false, "main");
+		VTIconStore.actions.newKeyframe("frequency", tMid, fVal, false, "main");
+	}
+
+	VTIconStore.actions.unselectKeyframes("main");
+	VTIconStore.actions.addSelectedKeyframes([0,1], "main");
+	VTIconStore.actions.deleteSelectedKeyframes("main");
+
+}
+
+
+
+/**
+ *  loadJSONFile takes the contents of a JSON file and loads up the saved
+ *   Macaron waveform into the Macaron editor.
+ **/
+var loadJSONFile = function(r) {
+	alert("it's a JSON file!");
+}
+
+
+/**
+ * estimateAmplitude estimates the average amplitude of a provided waveform
+ *  within a given chunk of a provided WAV file.
+ **/
+var estimateAmplitude = function(waveChunk) {
+	console.log(macaronMax(waveChunk)/Math.pow(2,8));
+	return macaronMax(waveChunk); //stub
+}
+
+
+
+/**
+ *  estimateFrequency uses a Fourier transform to estimate the average
+ *   frequency of a provided waveform in a certain region.
+ **/
+var estimateFrequency = function(waveChunk) {
+	return 250; // stub
+}
+
+
+/**
+ *  macaronMax finds the max value in an array (without exceeding the call
+ *   stack limit...)
+ **/
+var macaronMax = function(data) {
+	var dMax = 0;
+	for (var i=0; i<data.length; i++) {
+		if (data[i] > dMax) {dMax = data[i];}
+	}
+	return dMax;
 }
 
 
