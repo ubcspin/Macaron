109bfc5bcde73dad913aff27cd9aedecd5ce4a49
Ben <ben.clark456@gmail.com>
2016-06-12 11:49:27 -0700

    Got Macaron Mix to upload wave and JSON files! Big step...

--- app/stores/mixcontrolsstore.js
+++ app/stores/mixcontrolsstore.js
@@ -1,6 +1,7 @@
 import Reflux from 'reflux'
 
 var VTIconStore = require('./vticonstore.js');
+import {transform} from './../../thirdparty/fft.js';
 
 var MixControlActions = Reflux.createActions(
 
@@ -131,17 +132,73 @@ var MixControlStore = Reflux.createStore({
     }
   },
 
-  onLoadWaveform1: function(file) {
+  onLoadWaveform1: function() {
     document.getElementById("wav-1-file").click();
-    document.getElementById("wav-1-file").onchange = function() {
-      alert(document.getElementById("wav-1-file").files[0].name);
+    document.getElementById("wav-1-file").onchange = function(){
+      var file = document.getElementById("wav-1-file").files[0];
+      var reader = new FileReader();
+  		reader.filename = file.name;
+      // Case 1: It's a WAV file...
+  		if (reader.filename.indexOf('.wav') >= 0) {
+  			reader.onload = function(e) {
+  				var waveData = reader.result;
+  				if(MixControlStore._isWAVFile(reader, reader.filename)) {
+  					MixControlStore._loadWAVFile(reader, "wave1");
+  				}	else {
+  					alert('The selected file wasnt one that Macaron recognizes');
+  				}
+  			}
+  			reader.readAsArrayBuffer(file);
+  		}
+
+  		// Case 2: It's a JSON File or something else...
+  		else {
+  			reader.onload = function(e) {
+  				var waveData = reader.result;
+  				if (MixControlStore._isJSONFile(reader, reader.filename)) {
+  					VTIconStore.actions.setVTIcon(JSON.parse(waveData.slice(29)), "wave1");
+  				} else {
+  					alert('The selected file wasnt one that Macaron recognizes. Please upload an appropriate WAV or JSON file.');
+  				}
+  			}
+        reader.readAsArrayBuffer(file);
+      }
+
     };
   },
 
   onLoadWaveform2: function() {
     document.getElementById("wav-2-file").click();
-    document.getElementById("wav-2-file").onchange = function() {
-      alert(document.getElementById("wav-2-file").files[0].name);
+    document.getElementById("wav-2-file").onchange = function(){
+      var file = document.getElementById("wav-2-file").files[0];
+      var reader = new FileReader();
+  		reader.filename = file.name;
+      // Case 1: It's a WAV file...
+  		if (reader.filename.indexOf('.wav') >= 0) {
+  			reader.onload = function(e) {
+  				var waveData = reader.result;
+  				if(MixControlStore._isWAVFile(reader, reader.filename)) {
+  					MixControlStore._loadWAVFile(reader, "wave2");
+  				}	else {
+  					alert('The selected file wasnt one that Macaron recognizes');
+  				}
+  			}
+  			reader.readAsArrayBuffer(file);
+  		}
+
+  		// Case 2: It's a JSON File or something else...
+  		else {
+  			reader.onload = function(e) {
+  				var waveData = reader.result;
+  				if (MixControlStore._isJSONFile(reader, reader.filename)) {
+  					VTIconStore.actions.setVTIcon(JSON.parse(waveData.slice(29)), "wave2");
+  				} else {
+  					alert('The selected file wasnt one that Macaron recognizes. Please upload an appropriate WAV or JSON file.');
+  				}
+  			}
+        reader.readAsArrayBuffer(file);
+      }
+
     };
   },
 
@@ -153,7 +210,6 @@ var MixControlStore = Reflux.createStore({
     VTIconStore.actions.newKeyframe("frequency", 1, 400, name="mixedWave");
 
     VTIconStore.actions.selectAllKeyframes("mixedWave");
-    VTIconStore.actions.deleteSelectedKeyframes("mixedWave");
 
     for (var i=0; i<nKnots; i++) {
 
@@ -164,8 +220,6 @@ var MixControlStore = Reflux.createStore({
       var freq1 = this._getCurrentFrequency(t, "wave1");
       var freq2 = this._getCurrentFrequency(t, "wave2");
       var freq = ((0.01*amt) * freq1) + ((0.01*(100-amt)) * freq2);
-      console.log(freq1);
-      console.log(freq);
 
       VTIconStore.actions.selectTimeRange(i*windowWidth, (i+1)*windowWidth, "mixedWave");
       VTIconStore.actions.deleteSelectedKeyframes("mixedWave");
@@ -176,6 +230,7 @@ var MixControlStore = Reflux.createStore({
       VTIconStore.actions.unselectKeyframes("mixedWave");
     }
 
+    VTIconStore.actions.deleteSelectedKeyframes("mixedWave");
     VTIconStore.actions.selectKeyframesInRange(1499,1501,1,"mixedWave");
     VTIconStore.actions.deleteSelectedKeyframes("mixedWave");
 
@@ -234,8 +289,175 @@ var MixControlStore = Reflux.createStore({
 
   	}
 
-	return frequency;
-  }
+	   return frequency;
+  },
+
+  /**
+   *  isJSONFile determines if the provided file is, in fact, a JSON file
+   *   that can be understood by the Macaron app.
+   *
+   * @param r a JS Reader produced by the Filereader API when a file is loaded.
+   * @param fn a string representing the name of the uploaded file.
+   *
+   * @return a boolean, True if file is useable JSON, False otherwise.
+   **/
+  _isJSONFile: function(r, fn) {
+
+  	var fileExtOK = (fn.indexOf('.json') >= 0);
+
+  	try {
+  		var test = JSON.parse(r.result.slice(29));
+  		var fileContentOK = true;
+  	}
+  	catch(err) {
+  		var fileContentOK = false;
+  	}
+
+  	console.log(fileExtOK); console.log(fileContentOK);
+
+  	var fileOK = fileExtOK && fileContentOK;
+
+  	return(fileOK);
+  },
+
+
+
+  /**
+   * isWavFile determines whether or not a provided file is actually an
+   *  appropriate WAV file (with readable bit-depth, correct file format,
+   *   useful number of channels, etc.)
+   *
+   * @param r a JS Reader produced by the Filereader API with a file is loaded.
+   * @param fn a string representing the name of the uploaded file.
+   *
+   * @return a boolean, True if the file is a WAV file, False otherwise.
+   **/
+  _isWAVFile: function(r, fn) {
+
+  	var result = false;
+  	var wavedata = new DataView(r.result);
+
+  	var header = String.fromCharCode(wavedata.getInt8(0));  //R
+  	header +=    String.fromCharCode(wavedata.getInt8(1));  //I
+  	header +=    String.fromCharCode(wavedata.getInt8(2));  //F
+  	header +=    String.fromCharCode(wavedata.getInt8(3));  //F
+  	header +=    String.fromCharCode(wavedata.getInt8(8));  //W
+  	header +=    String.fromCharCode(wavedata.getInt8(9));  //A
+  	header +=    String.fromCharCode(wavedata.getInt8(10)); //V
+  	header +=    String.fromCharCode(wavedata.getInt8(11)); //E
+
+  	// Make sure we're actually looking at a WAV file.
+  	var headerOK = (header == "RIFFWAVE");
+  	var fileExOK = (fn.indexOf('.wav') >= 0);
+
+  	if (headerOK && fileExOK) {
+  		result = true;
+  	}
+
+  	return result;
+  },
+
+
+
+  /**
+   *  loadWAVFile takes the contents of a WAV file and loads an approximation
+   *   of that file's waveform into the Macaron editor. This function relies
+   *    heavily on the code written by the amazing Benson!
+   *
+   * @param r a JS Reader created by the Filereader API when a file is loaded.
+   **/
+  _loadWAVFile: function(r, editor) {
+
+  	var y;  // speaker displacement at time = t
+
+  	var sampleRate;
+  	var duration;
+  	var nChannels;
+  	var nFrames;
+  	var wavedata = r.result;
+
+  	var AudioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+  	AudioCtx.decodeAudioData(wavedata, function(buff) {
+  		sampleRate = buff.sampleRate;
+  		duration = buff.duration;
+  		nChannels = buff.numberOfChannels;
+  		nFrames = buff.length;
+
+  		var waveBuffer = new Array(nFrames);
+  		waveBuffer = buff.getChannelData(0); // Yup, just one channel...
+
+  		var nPannels = 40; // The number of points at which the input is to
+  											 // be estimated. (MUST BE DIVISIBLE BY 20).
+  		var pannelDuration = (duration * 1000) / nPannels; // in ms
+  		var pannelWidth = Math.round(nFrames / nPannels); // in number of frames
+
+  		for (var i=0; i<nPannels; i++) {
+
+  			var jMin = Math.round(pannelWidth * i);
+  			var jMax = Math.round(pannelWidth * (i + 1));
+
+  			var tMid = ((i*1000*duration)/nPannels)+((500*duration)/nPannels);
+
+  			var waveChunk = waveBuffer.slice(jMin, jMax);
+
+  			var aVal = Math.max.apply(null, waveChunk);
+
+  			var fVal = MixControlStore._findFFTRoot(waveChunk, sampleRate);
+
+
+  			VTIconStore.actions.newKeyframe("amplitude", tMid, aVal, false, editor);
+  			VTIconStore.actions.newKeyframe("frequency", tMid, fVal, false, editor);
+  		}
+
+  		VTIconStore.actions.unselectKeyframes(editor);
+  		VTIconStore.actions.addSelectedKeyframes([0,1], editor);
+  		VTIconStore.actions.deleteSelectedKeyframes(editor);
+  	});
+  },
+
+  /**
+   *  findFFTRoot computes the Discrete Fourier Transform of the audio data,
+   *   and takes the frequency with the largest weight to be the approximated
+   *    frequency of the entire data chunk. This function relies on the "fft"
+   *     javascript functions found in the Third Party directory.
+   *
+   * @param data an array of y values decoded from a WAV file.
+   * @param sampleRate a number representing the sample rate of the wave data.
+   *
+   * @return a number reresenting a best guess at the frequency
+   *          of the wave in the data.
+   **/
+  _findFFTRoot: function(data, sampleRate) {
+  	var real = new Array(data.length);
+  	var imag = new Array(data.length);
+
+  	for (var i=0; i<data.length; i++){
+  		real[i] = data[i]; imag[i] = 0;
+  	}
+
+  	transform(real, imag);
+  	// "transform" comes from the fft utility in the thirdparty directory.
+
+  	for (var i=0; i<data.length; i++) {
+  		real[i] = Math.abs(real[i]);
+  	}
+
+  	var halfReal = new Array(Math.round(data.length / 2));
+  	for (var i=0; i<data.length/2; i++) {
+  		halfReal[i] = real[i];
+  	}
+
+  	var fftRootLocation = real.indexOf(Math.max.apply(null, halfReal));
+
+  	var Fs = new Array(data.length);
+  	for (var i=1; i<=data.length; i++) {
+  		Fs[i] = i * sampleRate/data.length;
+  	}
+
+  	return Math.round(Fs[fftRootLocation]); //stub
+  },
+
 
 });
 
