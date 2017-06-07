2b721aa1327146b08eb12a4587ed7c4c487ebf0a
Oliver Schneider <oschneid@cs.ubc.ca>
2017-06-07 16:27:39 +0200

    Added CSS to mobile version

--- app/mobilepanel.jsx
+++ app/mobilepanel.jsx
@@ -9,8 +9,23 @@ var MobilePanel = React.createClass({
 
   propTypes: {
     parentLeft: React.PropTypes.String,
-    parentRight: React.PropTypes.String
-  },
+    parentRight: React.PropTypes.String,
+	name : React.PropTypes.string.isRequired,
+	selection : React.PropTypes.object.isRequired,
+	currentTime: React.PropTypes.number.isRequired
+	},
+
+	getDefaultProps: function() {
+	    return {
+	      height: 190,
+	      circleColor:'#FF8400',
+	      selectedCircleColor:'#B05B00',
+	      keyframeCircleRadius: 1,
+	      playheadFill: '#FF0000',
+
+	    }
+	},
+
 	
 
 	/**
@@ -20,6 +35,17 @@ var MobilePanel = React.createClass({
 
 		return (
 			<div className="mobilepanel">
+			<KeyframeEditor 
+					name="foo"
+                    height="160"
+                    axisNameWidth="20"
+    				scaleX={scaleXwave2}
+    				currentTime={this.state.playback.currentTime}
+    				parameter="amplitude"
+					vticon={this.props.parentLeft}
+    				keyframeCircleRadius={this.props.keyframeCircleRadius}
+					playheadFill={this.props.playheadFill}
+    				selection={this.state.selection}/>
 			</div>
 		);
 	}
